ae409673dfc967f626dabd499c3fd48c88c1fa84
Ramon Gilabert <ramon.gilabert.llop@gmail.com>
2015-12-27 12:32:37 +0100

    Adds some steps to do

---
+++ public/colors.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  function presentColor() {
+    // TODO: Change background color of the body and the text of the label.
+  }
+
+  function transformDate() {
+    // 1. Get the date.
+    // 2. Get the string from the date without the dots and stuff, so put hour, etc. in one string.
+    // 3. Create the string and return it.
+  }
+});
